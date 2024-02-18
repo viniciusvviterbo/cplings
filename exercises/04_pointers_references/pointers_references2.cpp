04fb8427a2b35aa202f17379e7e24baec22400c5
Vinícius Viterbo <viniciusvviterbo@protonmail.com>
2024-02-18 13:00:56 -0300

    resolution: 04_pointers_references - completed

--- exercises/04_pointers_references/pointers_references2.cpp
+++ exercises/04_pointers_references/pointers_references2.cpp
@@ -5,25 +5,25 @@
 // Make me compile! Go to the folder hint if you want a hint :)
 
 // We sometimes encourage you to keep trying things on a given exercise,
-// even after you already figured it out. 
+// even after you already figured it out.
 
 // Step 1: Make me compile!
 // Fix the syntax mistakes to return the correct reference values
 
-void my_reference_content(int value, int increment) {
-    value += increment; // Don't change this line
+void my_reference_content(int &value, const int increment) {
+  value += increment; // Don't change this line
 }
 
 #include <catch2/catch.hpp>
 
 TEST_CASE("my_reference_content") {
-    std::vector<int> values = { 1, 5, 10, 15 };
-    std::vector<int> increments = { -1, 1, -10, 3 };
+  std::vector<int> values = {1, 5, 10, 15};
+  std::vector<int> increments = {-1, 1, -10, 3};
 
-    for (size_t i = 0; i < values.size(); i++)
-    {
-        const int old_copy = values[i];
-        my_reference_content(values[i], increments[i]);
-        REQUIRE(values[i] == old_copy + increments[i]); // new_data_ptr must point to val
-    }
+  for (size_t i = 0; i < values.size(); i++) {
+    const int old_copy = values[i];
+    my_reference_content(values[i], increments[i]);
+    REQUIRE(values[i] ==
+            old_copy + increments[i]); // new_data_ptr must point to val
+  }
 }
